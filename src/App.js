65f6f91376a2ee828f244b67794c7558abf68437
Vince Bmmrt <vincebommert75@gmail.com>
2023-08-10 18:00:23 +0200

    gpt integration newTest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [botReply, setBotReply] = useState("");
+
+  const handleChat = async () => {
+    try {
+      const response = await axios.post("http://localhost:3001/chat", {
+        message,
+      });
+
+      setBotReply(response.data.reply);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chat Bot</h1>
+      <div>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={handleChat}>Send</button>
+      </div>
+      <p>{botReply}</p>
     </div>
   );
 }
